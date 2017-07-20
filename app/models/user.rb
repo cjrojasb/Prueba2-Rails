fd025d2c6c40b7ddca1976c957f4d86396fd0c59
Carlos Rojas <carlos.rojas.burgos@gmail.com>
2017-07-19 20:10:20 -0400

    3.1 commit - add capitalize name User

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,10 @@ class User < ApplicationRecord
   has_many :items
   validates :name, presence: true
   validates :email, uniqueness: true
+  before_save :capitalize
+
+  def capitalize
+    self.name.capitalize!
+  end
+
 end
